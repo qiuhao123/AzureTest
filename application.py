f16e2905b5381ee657c3640b7ce32244c882c75e
Qiuhao Jin <jinq@2msoe.edu>
2020-07-09 11:55:24 -0400

    change to hello worldt

--- application.py
+++ application.py
@@ -14,22 +14,22 @@ def home():
     #return render_template('home.html')
     return "hello world"
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [int_features]
-    #return type(''.join(final_features))
-    scoring_uri = 'http://43aa851b-4045-482b-9016-9f97cc6b3e70.westus2.azurecontainer.io/score'
-
-    #prediction = model.predict(final_features)
-    data = {"data":final_features}
-    #return ''.join(data)
-    input_data = json.dumps(data)
-    headers = {'Content-Type': 'application/json'}
-    resp = requests.post(scoring_uri, input_data, headers=headers)
-    prediction = "the value should be" + resp.text
-
-    #
-    return render_template('home.html', prediction_text= prediction)
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#     int_features = [int(x) for x in request.form.values()]
+#     final_features = [int_features]
+#     #return type(''.join(final_features))
+#     scoring_uri = 'http://43aa851b-4045-482b-9016-9f97cc6b3e70.westus2.azurecontainer.io/score'
+#
+#     #prediction = model.predict(final_features)
+#     data = {"data":final_features}
+#     #return ''.join(data)
+#     input_data = json.dumps(data)
+#     headers = {'Content-Type': 'application/json'}
+#     resp = requests.post(scoring_uri, input_data, headers=headers)
+#     prediction = "the value should be" + resp.text
+#
+#     #
+#     return render_template('home.html', prediction_text= prediction)
 if __name__ == "__main__":
     app.run(debug=True)
